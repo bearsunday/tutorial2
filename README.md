911829354c2c49670a33e961aaa947f6475c1e87
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-08-07 07:26:39 +0900

    init commit
    composer create-project bear/skeleton MyVendor.Ticket

---
+++ README.md
@@ -0,0 +1,19 @@
+# MyVendor.Ticket
+
+## Installation
+
+    composer install
+
+## Usage
+
+### Run server
+
+    composer serve
+
+### QA
+
+    composer test     // run unit test
+    composer tests    // run test and quality checks
+    composer coverage // test coverage
+    composer cs-fix   // fix the coding standard
+
---
+++ src/Module/AppModule.php
@@ -0,0 +1,18 @@
+<?php
+namespace MyVendor\Ticket\Module;
+
+use BEAR\Package\AbstractAppModule;
+use BEAR\Package\PackageModule;
+
+class AppModule extends AbstractAppModule
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $appDir = $this->appMeta->appDir;
+        require_once $appDir . '/env.php';
+        $this->install(new PackageModule);
+    }
+}
