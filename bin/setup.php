7278e7f2cc22cbeeb87a210a57ff1c73f831ea11
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-08-07 07:46:38 +0900

    DB migration

--- bin/setup.php
+++ bin/setup.php
@@ -1,5 +1,14 @@
 <?php
+require dirname(__DIR__) . '/autoload.php';
+require_once dirname(__DIR__) . '/env.php';
+// dir
 chdir(dirname(__DIR__));
 passthru('rm -rf var/tmp/*');
 passthru('chmod 775 var/tmp');
 passthru('chmod 775 var/log');
+// db
+$pdo = new \PDO('mysql:host=' . getenv('TKT_DB_HOST'), getenv('TKT_DB_USER'), getenv('TKT_DB_PASS'));
+$pdo->exec('CREATE DATABASE IF NOT EXISTS ' . getenv('TKT_DB_NAME'));
+$pdo->exec('CREATE DATABASE IF NOT EXISTS ' . getenv('TKT_DB_NAME') . '_test');
+passthru('./vendor/bin/phinx migrate -c var/phinx/phinx.php -e development');
+passthru('./vendor/bin/phinx migrate -c var/phinx/phinx.php -e test');
