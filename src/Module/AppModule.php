f573a133fad655ac4292a432326abf2a66e1e548
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-08-07 07:30:53 +0900

    install modules

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -3,6 +3,12 @@ namespace MyVendor\Ticket\Module;
 
 use BEAR\Package\AbstractAppModule;
 use BEAR\Package\PackageModule;
+use BEAR\Package\Provide\Router\AuraRouterModule;
+use BEAR\Resource\Module\JsonSchemaLinkHeaderModule;
+use BEAR\Resource\Module\JsonSchemaModule;
+use Ray\AuraSqlModule\AuraSqlModule;
+use Ray\IdentityValueModule\IdentityValueModule;
+use Ray\Query\SqlQueryModule;
 
 class AppModule extends AbstractAppModule
 {
@@ -13,6 +19,24 @@ class AppModule extends AbstractAppModule
     {
         $appDir = $this->appMeta->appDir;
         require_once $appDir . '/env.php';
+        $this->install(
+            new AuraSqlModule(
+                getenv('TKT_DB_DSN'),
+                getenv('TKT_DB_USER'),
+                getenv('TKT_DB_PASS'),
+                getenv('TKT_DB_SLAVE')
+            )
+        );
+        $this->install(new SqlQueryModule($appDir . '/var/sql'));
+        $this->install(new IdentityValueModule);
+        $this->install(
+            new JsonSchemaModule(
+                $appDir . '/var/json_schema',
+                $appDir . '/var/json_validate'
+            )
+        );
+        $this->install(new JsonSchemaLinkHeaderModule('http://www.example.com/'));
+        $this->install(new AuraRouterModule($appDir . '/var/conf/aura.route.php'));
         $this->install(new PackageModule);
     }
 }
