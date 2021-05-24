45e043bfb9b0b3d3f3b03f9e38af529491220d80
koriym <akihito.koriyama@gmail.com>
2021-05-24 11:04:35 +0900

    Embed /project into ticket resource

--- src/Resource/App/Ticket.php
+++ src/Resource/App/Ticket.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace MyVendor\Ticket\Resource\App;
 
+use BEAR\Resource\Annotation\Embed;
 use BEAR\Resource\Annotation\JsonSchema;
+use BEAR\Resource\Request;
 use BEAR\Resource\ResourceObject;
 use MyVendor\Ticket\Query\TicketQueryInterface;
+use function assert;
 
 class Ticket extends ResourceObject
 {
@@ -15,10 +18,12 @@ class Ticket extends ResourceObject
     ) {
     }
 
+    #[Embed(src: '/project', rel: 'project')]
     #[JsonSchema('ticket.json')]
     public function onGet(string $id = ''): static
     {
-        $this->body = $this->query->item($id);
+        assert($this->body['project'] instanceof Request);
+        $this->body += $this->query->item($id);
 
         return $this;
     }
