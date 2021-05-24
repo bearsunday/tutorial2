e8e357c8324fa6e25b8239b9ce0263e49ef3c5b6
koriym <akihito.koriyama@gmail.com>
2021-05-24 10:59:25 +0900

    Migration

--- bin/setup.php
+++ bin/setup.php
@@ -2,5 +2,24 @@
 
 declare(strict_types=1);
 
+use BEAR\Dotenv\Dotenv;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+(new Dotenv())->load(dirname(__DIR__));
 chdir(dirname(__DIR__));
-passthru('rm -rf ./var/tmp/*');
+
+// dir
+chdir(dirname(__DIR__));
+passthru('rm -rf var/tmp/*');
+passthru('chmod 775 var/tmp');
+passthru('chmod 775 var/log');
+
+// db
+$pdo = new PDO('mysql:host=' . getenv('TKT_DB_HOST'), getenv('TKT_DB_USER'), getenv('TKT_DB_PASS'));
+$pdo->exec('DROP DATABASE IF EXISTS ' . getenv('TKT_DB_NAME'));
+$pdo->exec('DROP DATABASE IF EXISTS ' . getenv('TKT_DB_NAME')) . '_test';
+$pdo->exec('CREATE DATABASE IF NOT EXISTS ' . getenv('TKT_DB_NAME'));
+$pdo->exec('CREATE DATABASE IF NOT EXISTS ' . getenv('TKT_DB_NAME') . '_test');
+passthru('./vendor/bin/phinx migrate -c var/phinx/phinx.php -e development');
+passthru('./vendor/bin/phinx migrate -c var/phinx/phinx.php -e test');
